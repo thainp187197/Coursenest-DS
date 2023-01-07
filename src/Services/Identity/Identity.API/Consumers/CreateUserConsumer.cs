147d3fee6acc495dc270f460cc9126489b5c1eb3
thainp187197 <thai.np187197@sis.hust.edu.vn>
2023-01-07 09:23:28 +0700

    Added MassTransit consumer. Updated Automapper mapping profiles.

---
+++ src/Services/Identity/Identity.API/Consumers/CreateUserConsumer.cs
@@ -0,0 +1,35 @@
+﻿using APICommonLibrary.MessageBus.Commands;
+using AutoMapper;
+using Identity.API.Infrastructure.Contexts;
+using Identity.API.Infrastructure.Entities;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.API.Consumers;
+
+public class CreateUserConsumer : IConsumer<CreateUser>
+{
+	private readonly IMapper _mapper;
+	private readonly DataContext _context;
+
+	public CreateUserConsumer(IMapper mapper, DataContext context)
+	{
+		_mapper = mapper;
+		_context = context;
+	}
+
+	public async Task Consume(ConsumeContext<CreateUser> context)
+	{
+		var exist = await _context.Users.AnyAsync(x => x.Email == context.Message.Email);
+		if (exist) throw new ArgumentException("Email existed.");
+
+		var user = _mapper.Map<User>(context.Message);
+
+		_context.Users.Add(user);
+		await _context.SaveChangesAsync();
+
+		var result = new CreateUserResult() { UserId = user.UserId };
+
+		await context.RespondAsync(result);
+	}
+}
---
+++ src/Services/Identity/Identity.API/MappingProfiles/DefaultProfile.cs
@@ -0,0 +1,50 @@
+﻿using APICommonLibrary.MessageBus.Commands;
+using AutoMapper;
+using Identity.API.DTOs;
+using Identity.API.Infrastructure.Entities;
+
+namespace Identity.API.MappingProfiles;
+
+public class DefaultProfile : Profile
+{
+	public DefaultProfile()
+	{
+		// User
+		CreateMap<CreateUser, User>()
+			.ForMember(dest => dest.Created, opt => opt.MapFrom(_ => DateTime.Now))
+			.ForMember(dest => dest.LastModified, opt => opt.MapFrom(_ => DateTime.Now))
+			.ForMember(
+				dest => dest.InterestedTopics,
+				opt => opt.MapFrom(x => x.InterestedTopicIds
+					.Select(i => new InterestedTopic() { TopicId = i })));
+		CreateMap<UpdateUser, User>()
+			.ForMember(dest => dest.LastModified, opt => opt.MapFrom(_ => DateTime.Now))
+			.ForAllMembers(options =>
+			{
+				options.Condition((source, destination, member) => member != null);
+			});
+
+		CreateMap<User, UserResult>();
+		CreateMap<User, UserProfileResult>();
+		CreateMap<User, UserInstructorResult>();
+
+		// Avatar
+		CreateMap<Avatar, ImageResult>()
+			.ForMember(
+				nameof(ImageResult.URI),
+				options => options.MapFrom(src => $"data:{src.MediaType};base64,{Convert.ToBase64String(src.Data)}"));
+
+		// InterestedTopic
+		CreateMap<InterestedTopic, int>()
+			.ConvertUsing(x => x.TopicId);
+
+		// FollowedTopic
+		CreateMap<FollowedTopic, int>()
+			.ConvertUsing(x => x.TopicId);
+
+		// Experience
+		CreateMap<CreateExperience, Experience>();
+
+		CreateMap<Experience, ExperienceResult>();
+	}
+}
